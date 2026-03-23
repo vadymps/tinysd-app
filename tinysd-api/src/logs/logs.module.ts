827792ccfd726c0c345abc3f9b9ed3b1344cc021
Vadym Pastushenko <vadym.pastushenko7@gmail.com>
2026-03-23 11:03:15 +0100

    Revert changes

--- tinysd-api/src/logs/logs.module.ts
+++ tinysd-api/src/logs/logs.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LogsController } from './controllers/logs.controller';
 import { LogsService } from './services/logs.service';
+import { DatabaseModule } from '../database/database.module';
+
 @Module({
+  imports: [DatabaseModule],
   controllers: [LogsController],
   providers: [LogsService],
   exports: [LogsService],
